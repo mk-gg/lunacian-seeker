6e9934759f1a66e00aa11626a1c0fdc36917a27b
mk-greg <mk.gregy@gmail.com>
2022-10-09 16:19:43 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Lunacian Seeker
+This tool aims to check whether if a community member is eligible for rewards. 
+The site can be found [here](https://lunacian-seeker.herokuapp.com/)
+
+![](https://i.imgur.com/ETgS9qv.gif)
---
+++ main.py
@@ -0,0 +1,39 @@
+#app/main.py
+import json
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+@app.route("/")
+def home_view():
+    return render_template(
+        'profile.html', data=[])
+
+@app.route("/result", methods=['GET', 'POST'])
+def result():
+    data = []
+    error = None
+    select = request.form.get('comp_select')
+    
+    resp = requests.get('https://ronin.rest/sm/resolveProfile/'+select)
+    jsonResp = resp.json()
+    
+    if 'error' in jsonResp:
+        return render_template('error.html',data=data,error=error)
+    
+    data.append(jsonResp)
+    
+    with open("Season0.json",'r', encoding="utf8") as jsonFile:
+        dataResp = json.load(jsonFile)
+        jsonData = dataResp["_items"]
+        
+    result = list(filter(lambda jsonData: jsonData['id'] == jsonResp["accountId"], jsonData))
+    
+    data = data + result
+    
+    return render_template(
+        'result.html', data=data)
+    
+if __name__== '__main__':
+    app.run(debug=True)
